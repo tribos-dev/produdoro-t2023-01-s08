f27016249eb28c8d0e82d9a5ee6354bf899d0c9e
= <gabriel.cirilo.rosa@gmail.com>
2023-08-29 21:48:24 -0300

    implementacao teste unitario

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,40 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
 
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioApplicationService usuarioApplicationService;
+
+    @Test
+    @DisplayName("Teste unitário pausa longa")
+    void pausaLongaTest() {
+        UUID idUsuario = UUID.randomUUID();
+        Usuario usuario = Usuario.builder()
+                .idUsuario(idUsuario)
+                .email("cirilo@gmail.com")
+                .build();
+
+        when(usuarioRepository.buscaUsuarioPorId(idUsuario)).thenReturn(usuario);
+        usuarioApplicationService.pausaLonga("cirilo@gmail.com", idUsuario);
+        verify(usuarioRepository).buscaUsuarioPorId(idUsuario);
+        verify(usuarioRepository).salva(usuario);
+    }
 }
